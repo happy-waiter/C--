76c136549b9b0074ae9752b7b3a702455e99866c
happy-waiter <100430660+happy-waiter@users.noreply.github.com>
2022-02-27 20:38:36 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a, b, c;
+	int max(int x,int y);
+	scanf_s("%d,%d",&a,&b);
+	c = max(a, b);
+	printf("%d",c);
+}
+int max(int x, int y)
+{
+	int z;
+	if (x > y)z = x;
+	else z = y;
+	return(z);
+}
