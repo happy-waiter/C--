669355cddd88ead2ed535dd6f95217c6bb24f5ee
happy-waiter <100430660+happy-waiter@users.noreply.github.com>
2022-03-06 23:40:06 +0800

    Add files via upload

---
+++ 新的开始练习.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int* p1, * p2,*p,a,b;
+	scanf_s("%d,%d",& a, &b);
+	p1 = &a;
+	p2 = &b;
+	if(a<b)
+	{
+		p = p1; p1 = p2; p2 = p;
+	}
+	printf("a=%d,b=%d\n", *p1, *p2);
+	return 0;
+
+}
