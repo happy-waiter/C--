46fa52fddb0599e169f2a9d8c02e3b3ff2ae035b
happy-waiter <100430660+happy-waiter@users.noreply.github.com>
2022-03-10 22:57:35 +0800

    Add files via upload

---
+++ 源2.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int A;
+int main()
+{
+	int power(int);
+	int b = 3, c, d, m;
+	printf("enter the number aand its power");
+	scanf("%d,%d", &A, &m);
+	c = A * b;
+	printf("%d*%d")
+}
